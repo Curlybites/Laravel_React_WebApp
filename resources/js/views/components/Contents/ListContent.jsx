c55450bfe685aa458dab6fd119588ce5bb2bbb6c
Alphy <alphy.balatucan16@gmail.com>
2024-06-27 22:00:22 +0800

    current listcontent

--- resources/js/views/components/Contents/ListContent.jsx
+++ resources/js/views/components/Contents/ListContent.jsx
@@ -1,9 +1,136 @@
-import React from 'react'
+import React from "react";
 
 export default function ListContent() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div class="mx-16 my-8">
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div class="">
+                    <div class="card bg-base-100">
+                        <figure className="w-2/2 h-80s">
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                                alt="Shoes"
+                                className=""
+                            />
+                        </figure>
+                        <div className="card-body pt-4">
+                            <h2 className="font-bold text-3xl mt-5">
+                                The Main Article
+                            </h2>
+                            <p className="text-blue-500 mt-2.5">catergory</p>
+                            <p className="mt-2.5">
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industry's standard dummy text ever since
+                                the 1500s, when an unknown printer took a galley
+                                of type and scrambled it to make a type specimen
+                                book. It has survived not only five centuries,
+                                but also the leap into electronic typesetting,
+                                remaining essentially unchanged.
+                            </p>
+                            <div className="author mt-2.5 flex items-center ">
+                                <div className="avatar">
+                                    <div className="h-12 w-12 mask mask-squircle mt-2.5">
+                                        <img src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg" />
+                                    </div>
+                                </div>
+                                <div className="author-details mx-2.5 ">
+                                    <p className="mt-1 text-blue-500 text-sm">
+                                        Anthony Esquilona
+                                    </p>
+                                    <p className="text-xs mt-1 text-slate-400">
+                                        Jun 23
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="card bg-base-100">
+                        <figure className="w-2/2 h-80s">
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                                alt="Shoes"
+                                className=""
+                            />
+                        </figure>
+                        <div className="card-body pt-4">
+                            <h2 className="font-bold text-3xl mt-5">
+                                The Main Article
+                            </h2>
+                            <p className="text-blue-500 mt-2.5">catergory</p>
+                            <p className="mt-2.5">
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industry's standard dummy text ever since
+                                the 1500s, when an unknown printer took a galley
+                                of type and scrambled it to make a type specimen
+                                book. It has survived not only five centuries,
+                                but also the leap into electronic typesetting,
+                                remaining essentially unchanged.
+                            </p>
+                            <div className="author mt-2.5 flex items-center ">
+                                <div className="avatar">
+                                    <div className="h-12 w-12 mask mask-squircle mt-2.5">
+                                        <img src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg" />
+                                    </div>
+                                </div>
+                                <div className="author-details mx-2.5 ">
+                                    <p className="mt-1 text-blue-500 text-sm">
+                                        Anthony Esquilona
+                                    </p>
+                                    <p className="text-xs mt-1 text-slate-400">
+                                        Jun 23
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="card bg-base-100">
+                        <figure className="w-2/2 h-80s">
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                                alt="Shoes"
+                                className=""
+                            />
+                        </figure>
+                        <div className="card-body pt-4">
+                            <h2 className="font-bold text-3xl mt-5">
+                                The Main Article
+                            </h2>
+                            <p className="text-blue-500 mt-2.5">catergory</p>
+                            <p className="mt-2.5">
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industry's standard dummy text ever since
+                                the 1500s, when an unknown printer took a galley
+                                of type and scrambled it to make a type specimen
+                                book. It has survived not only five centuries,
+                                but also the leap into electronic typesetting,
+                                remaining essentially unchanged.
+                            </p>
+                            <div className="author mt-2.5 flex items-center ">
+                                <div className="avatar">
+                                    <div className="h-12 w-12 mask mask-squircle mt-2.5">
+                                        <img src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg" />
+                                    </div>
+                                </div>
+                                <div className="author-details mx-2.5 ">
+                                    <p className="mt-1 text-blue-500 text-sm">
+                                        Anthony Esquilona
+                                    </p>
+                                    <p className="text-xs mt-1 text-slate-400">
+                                        Jun 23
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
