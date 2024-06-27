89c9b975f137c9576d27feb4a52e9b01c54a4dbf
Anthony Esquilona <91820043+Curlybites@users.noreply.github.com>
2024-06-27 14:02:18 +0800

    Registration

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./views/Homepage";
 import Login from "./views/Login";
+import Register from "./views/Register";
 
 export default function app() {
     return (
@@ -9,6 +10,7 @@ export default function app() {
             <Routes>
                 <Route path="/" element={<Homepage />} />
                 <Route path="/Login" element={<Login />} />
+                <Route path="/Register" element={<Register/>} />
             </Routes>
         </BrowserRouter>
     );
--- resources/js/views/Register.jsx
+++ resources/js/views/Register.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import { FaPenNib } from "react-icons/fa";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 export default function Register() {
-  return (
-    <div>
-      <h1>Register</h1>
-    </div>
-  )
+    useEffect(() => {
+        themeChange(false);
+        // 👆 false parameter is required for react project
+    }, []);
+    return (
+        <div className="flex items-center justify-center h-screen w-full">
+            <Link
+                to={"/"}
+                className="absolute top-0 left-0 btn btn-ghost rounded-full m-2"
+            >
+                <FaArrowLeft />
+            </Link>
+            <div className="content flex flex-col gap-10 mb-20 ">
+                <div className="logo flex items-center justify-center text-4xl">
+                    <FaPenNib />
+                    <span className="font-bold">Blogg.</span>
+                </div>
+
+                <button className="border-2 border-current rounded-lg p-2">
+                    Sign up with Google
+                </button>
+
+                <div className="divider">or</div>
+
+                <form action="" className="flex flex-col gap-3">
+                    <input
+                        type="Email"
+                        placeholder="Email"
+                        required
+                        className="input input-bordered w-full max-w-xs "
+                    />
+                    <input
+                        type="Password"
+                        placeholder="Password"
+                        required
+                        className="input input-bordered w-full max-w-xs"
+                    />
+                        <input
+                        type="Password"
+                        placeholder="Confirm Password"
+                        required
+                        className="input input-bordered w-full max-w-xs"
+                    />
+
+                    <button className="btn btn-primary text-white">
+                       Register 
+                    </button>
+                </form>
+
+                <Link to={"/Login"} className="text-center text-sm">
+                    have an account?{" "}
+                    <span className="text-blue-500">Sign In</span>
+                </Link>
+            </div>
+        </div>
+    );
 }
